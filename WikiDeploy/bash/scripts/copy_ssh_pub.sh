effb42716543ae02e3a053cbe603ace98c57eaa2
fueller-max <ovchinnikov.EL@yandex.ru>
2025-11-14 11:17:44 +0000

    Monitoring prem. finished

--- WikiDeploy/bash/scripts/copy_ssh_pub.sh
+++ WikiDeploy/bash/scripts/copy_ssh_pub.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 #Script to copy ssh pub key for all hosts
 
-echo -e "Starting copying ssh pub keys to all hosts...\n"
-
 HOSTS_FILE="hosts"
 SSH_PASS='Test123'
 
-# Check if the file hosts exists
+
+# if specific address has been provided ($1 == IP address)
+
+if [[ -n "$1" ]]; then
+  echo "provided IP address: $1"
+  sshpass -p "$SSH_PASS" ssh-copy-id -i ~/.ssh/id_rsa.pub user@$1
+else 
+  # if specific IP was not provided loop through the file line by line from hosts file
+  echo "No specific IP was provided. Perfoming reading from hosts file..."
+
+  # Check if the file hosts exists
 if [[ ! -f "$HOSTS_FILE" ]]; then
     echo "Error: File '$HOSTS_FILE' not found."
     exit 1
 fi
 
-# Loop through the file line by line
-while IFS= read -r ip; do
+  while IFS= read -r ip; do
     # Process each line here
     echo "IP: $ip"                                                     # output IP to console
     sshpass -p "$SSH_PASS" ssh-copy-id -i ~/.ssh/id_rsa.pub user@$ip   # copy ssh pub keys to remote hosts
 done < "$HOSTS_FILE"
 
 echo -e "Copying ssh pub keys to all hosts finished..."
+fi
+
 
 echo $?
